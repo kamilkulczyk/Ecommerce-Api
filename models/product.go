f20cf749dfd52a6316d50ec1b02b124fa05407e5
.NET backend developer, self taught <61236761+kamilkulczyk@users.noreply.github.com>
2025-02-26 15:58:08 +0000

    update product model

--- models/product.go
+++ models/product.go
@@ -1,11 +1,13 @@
 package models
 
 type Product struct {
-		ID       int     `json:"id"`
-		Name     string  `json:"name"`
-		Price    float64 `json:"price"`
-		Stock    int     `json:"stock"`
-		StatusID int     `json:"status_id"`
+	ID             int      `json:"id"`
+	Name           string   `json:"name"`
+	Price          float64  `json:"price"`
+	Stock          int      `json:"stock"`
+	StatusID       int      `json:"status_id"`
+	Description    string   `json:"description"`
+	Images         []string `json:"images"`
 }
 
 type ProductStatus struct {
